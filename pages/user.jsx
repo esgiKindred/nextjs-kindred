a8a4e022312218a6b911829dc51126bfecff246b
Pierre Landry <45065999+Linkale@users.noreply.github.com>
2022-06-28 08:14:56 +0200

    profil enfant

--- pages/user.jsx
+++ pages/user.jsx
@@ -11,22 +11,62 @@ import Link from "next/link";
 import Dashboard from "./index";
 import styles from "./user.module.css";
 import CardPoint from "../components/card-point/card-point";
+import { useEffect, useState } from "react";
+import { useSession } from "next-auth/react";
+import Circle from "../components/circles/circle";
 
 export default function Contract() {
+  const [userInformation, setUserInformation] = useState([]);
+  const [parentInformation, setParentInformation] = useState([])
+  const { data: session, status } = useSession();
+
+  useEffect(() => {
+    const fetchUser = async () => {
+        const response = await fetch(
+        "http://127.0.0.1:8000/api/" + "users/" + session.user.id,
+        {
+            headers: {
+            Accept: "application/json",
+            },
+        }
+        );
+        const newData = await response.json();
+        setUserInformation(newData);
+    };
+    fetchUser();
+
+    const fetchParent = async () => {
+      const response = await fetch(
+      "http://127.0.0.1:8000/api/" + "users/?enfants=" + session.user.id,
+      {
+          headers: {
+          Accept: "application/json",
+          },
+      }
+      );
+      const newData = await response.json();
+      console.log(newData)
+      setParentInformation(newData);
+    };
+    fetchParent();
+  }, [session.user.id]);
   return (
     <Container className={styles.main}>
       <div className={styles.picture_block}>
-        <Image src="" alt="Profile picture" className={styles.picture} />
+        <div class="d-flex justify-content-center"><Circle/></div>
       </div>
       <Block>
-        <h2>Prenom Nom</h2>
-        <span>email</span>
+        <h2>{userInformation.lastName} {userInformation.firstName}</h2>
+        <span>{userInformation.email}</span>
       </Block>
-      <Block className={styles.parent}>
+      <Block className={styles.children}>
         <h3>Enfant de :</h3>
-        <Block className={styles.parent_block}>
-          <Image src="" alt="Parent profile picture" />
-          <span>Prenom Nom</span>
+        <Block>
+          {parentInformation.map((parent) => {
+            return (
+              <div class="d-flex justify-content-center"><span><Circle/>{parent.lastName} {parent.firstName}</span></div>
+            );
+        })}
         </Block>
       </Block>
       <Block className={styles.points}>
@@ -34,12 +74,12 @@ export default function Contract() {
         <Block className={styles.points_block}>
             <CardPoint
                 title={"Total des Kin's"}
-                points={"- points"}
+                points={userInformation.kins}
                 type={'kins'}
             ></CardPoint>
             <CardPoint
                 title={"Total des points"}
-                points={"- points bonus"}
+                points={userInformation.pointsBonus}
                 type={'bonus'}
             ></CardPoint>
 
