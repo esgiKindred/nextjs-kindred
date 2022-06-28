bac87c09efda9bc5fffe5fbc3d799c4f2a9a2ce1
Carlitos73490 <71330622+Carlitos73490@users.noreply.github.com>
2022-06-28 17:40:56 +0200

    Logo Enfant/Parent in session.js + Clean

--- components/session/session.js
+++ components/session/session.js
@@ -1,27 +1,20 @@
 import React from "react";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { Button, Form } from "react-bulma-components";
-import Link from "next/link";
-import { useEffect, useState } from "react";
+
+import enfant from "../../assets/images/enfant1.png";
+import parent from "../../assets/images/parent1.png";
+import Image from "next/image";
 
 export default function Session() {
   const { data: session, status } = useSession();
-  const [profil, setProfil] = useState();
-  useEffect(() => {
-    if (session.user.roles.includes("parent")) {
-      setProfil(<a href="/user-parent">{session.user.email}</a>);
-    }
-
-    if (
-      session.user.roles.includes("enfant") ||
-      session.user.roles.includes("ROLE_CHILD")
-    ) {
-      setProfil(<a href="/user">{session.user.email}</a>);
-    }
-  }, []);
+  const isParent = session.user.roles.includes("parent");
   return (
     <div className="row">
-      <div className="d-flex justify-content-center">{profil}</div>
+      <div className="d-flex justify-content-center">
+          <Image src={isParent ? parent : enfant} alt="logo" width={40} height={40} />
+          <a href={ isParent? "/user-parent" :"/user"}>{session.user.email}</a>
+      </div>
       <Button
         className=""
         type="button"
