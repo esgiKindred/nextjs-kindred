2638ac30c65d090e1abfa1a3ee30bd1fb8bce875
Quentin Hiribarne <39909159+salatiiga@users.noreply.github.com>
2022-06-28 11:35:52 +0200

    hide buttons for child

--- pages/mission.jsx
+++ pages/mission.jsx
@@ -5,15 +5,17 @@ import Link from "next/link";
 import CardMissionsParent from "../components/card-missions-parent/card-missions-parent";
 import { useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
-import {GetMissionByUserId} from "../swr/service";
+import { GetMissionByUserId } from "../swr/service";
 
 export default function Mission() {
-
   const { data: session } = useSession();
-  const { data : missionsData, error : missionsError } = GetMissionByUserId(session.user.id)
+  const { data: missionsData, error: missionsError } = GetMissionByUserId(
+    session.user.id
+  );
+  const role = session.user.roles[0];
 
-    if (missionsError) return <h1>Something went wrong!</h1>
-    if (!missionsData) return <h1>Loading...</h1>
+  if (missionsError) return <h1>Something went wrong!</h1>;
+  if (!missionsData) return <h1>Loading...</h1>;
 
   return (
     <Container className={styles.main}>
@@ -21,29 +23,26 @@ export default function Mission() {
         <Columns.Column size={8} className={styles.title}>
           <h2 className={styles.h2}>Mes missions</h2>
         </Columns.Column>
-        <Columns.Column size={4} className={styles.boutons}>
-          <Link href={"/add-mission"} passHref>
-            <Button className={styles.bouton} type="button" color="secondary">
-              Créer une mission
-            </Button>
-          </Link>
-          <Link href={"/category"} passHref>
-            <Button className={styles.bouton} type="button" color="secondary">
-              Gérer les catégories
-            </Button>
-          </Link>
-        </Columns.Column>
+        {role == "parent" ? (
+          <Columns.Column size={4} className={styles.boutons}>
+            <Link href={"/add-mission"} passHref>
+              <Button className={styles.bouton} type="button" color="secondary">
+                Créer une mission
+              </Button>
+            </Link>
+            <Link href={"/category"} passHref>
+              <Button className={styles.bouton} type="button" color="secondary">
+                Gérer les catégories
+              </Button>
+            </Link>
+          </Columns.Column>
+        ) : null}
       </Columns>
       <Columns centered={true}>
-
-          {missionsData.map((mission) => {
-              return (
-                  <CardMissionsParent key={mission.id} mission={mission}/>
-              )
-          })
-          }
+        {missionsData.map((mission) => {
+          return <CardMissionsParent key={mission.id} mission={mission} />;
+        })}
       </Columns>
-
     </Container>
   );
 }
