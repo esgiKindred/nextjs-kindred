d76bcd84931237b5237c7ca003fb7ffba8cd1192
Quentin Hiribarne <39909159+salatiiga@users.noreply.github.com>
2022-06-28 10:27:14 +0200

    Ajout de la key pour chaque enfant

--- pages/user-parent.jsx
+++ pages/user-parent.jsx
@@ -15,55 +15,62 @@ import { useSession } from "next-auth/react";
 import Circle from "../components/circles/circle";
 
 export default function Contract() {
-    const [userInformation, setUserInformation] = useState([]);
-    const [childrenInformation, setChildrenInformation] = useState([])
-    const { data: session, status } = useSession();
+  const [userInformation, setUserInformation] = useState([]);
+  const [childrenInformation, setChildrenInformation] = useState([]);
+  const { data: session, status } = useSession();
 
-    useEffect(() => {
-      const fetchUser = async () => {
-          const response = await fetch(
-          "http://127.0.0.1:8000/api/" + "users/" + session.user.id,
-          {
-              headers: {
-              Accept: "application/json",
-              },
-          }
-          );
-          const newData = await response.json();
-          setUserInformation(newData);
-      };
-      fetchUser();
+  useEffect(() => {
+    const fetchUser = async () => {
+      const response = await fetch(
+        "http://127.0.0.1:8000/api/" + "users/" + session.user.id,
+        {
+          headers: {
+            Accept: "application/json",
+          },
+        }
+      );
+      const newData = await response.json();
+      setUserInformation(newData);
+    };
+    fetchUser();
 
-      const fetchChildren = async () => {
-        const response = await fetch(
+    const fetchChildren = async () => {
+      const response = await fetch(
         "http://127.0.0.1:8000/api/" + "users/?parent=" + session.user.id,
         {
-            headers: {
+          headers: {
             Accept: "application/json",
-            },
+          },
         }
-        );
-        const newData = await response.json();
-        setChildrenInformation(newData);
-      };
-      fetchChildren();
-    }, [session.user.id]);
+      );
+      const newData = await response.json();
+      setChildrenInformation(newData);
+    };
+    fetchChildren();
+  }, [session.user.id]);
   return (
     <Container className={styles.main}>
-      <div class="d-flex justify-content-center"><Circle/></div>
-      
+      <div className="d-flex justify-content-center">
+        <Circle />
+      </div>
+
       <Block>
-        <h2>{userInformation.lastName} {userInformation.firstName}</h2>
+        <h2>
+          {userInformation.lastName} {userInformation.firstName}
+        </h2>
         <span>{userInformation.email}</span>
       </Block>
       <Block className={styles.children}>
         <h3>Parent de :</h3>
         <Block className={styles.children_block}>
-        {childrenInformation.map((child) => {
+          {childrenInformation.map((child) => {
             return (
-            <span><Circle/>{child.lastName} {child.firstName}</span>
+              <span key={child.id}>
+                <Circle />
+                {child.lastName} {child.firstName}
+              </span>
             );
-        })}
+          })}
         </Block>
       </Block>
       <Block className={styles.buttons}>
